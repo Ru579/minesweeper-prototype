562c92954b4565d61da324ae5e043eca31798c14
Ru579 <shae.hewadewa@gmail.com>
2025-01-19 22:09:34 +0000

    Adding confused option

--- GUI.py
+++ GUI.py
@@ -7,33 +7,54 @@ root = Tk()
 
 
 def open_cell(tile,x,y):
-    tile.config(state = DISABLED)
+    tile.config(state = DISABLED, text=game.grid[x][y])
     if game.grid[x][y]=="*":
         print("GAME OVER!")
+        tile.config(bg = "red")
+    else:
+        tile.config(bg="white")
 
 def flag_cell(tile,x,y):
-    if tile["state"]!=DISABLED:
+    if tile["state"]!=DISABLED and cell_states[x][y]!="Flagged":
         print(f"Tile ({x},{y}) flagged")
-        tile.config(bg="blue")
+        tile.config(bg="blue",text="")
+        cell_states[x][y]="Flagged"
+    else:
+        print(f"Tile ({x},{y}) unflagged")
+        tile.config(bg="#f0f0f0")
+        cell_states[x][y] = "Unflagged"
+
+def confuse_cell(tile,x,y):
+    if tile["state"]!=DISABLED and cell_states[x][y]!="Confused":
+        print(f"Tile ({x},{y}) marked as a question")
+        tile.config(bg="green", text="?")
+        cell_states[x][y]="Confused"
+    else:
+        print(f"Tile ({x},{y}) no longer confused")
+        tile.config(bg="#f0f0f0", text="")
+        cell_states[x][y] = "Unflagged"
 
 
 
 
-game=Game()
+game=Game(16,50)
 
 
 tiles=[["BLANK" for _ in range(0,game.grid_size)] for _ in range(0,game.grid_size)]
+cell_states=[["Unflagged" for _ in range(0,game.grid_size)] for _ in range(0,game.grid_size)]
+
 
 for i in range(0,game.grid_size):
     for j in range(0,game.grid_size):
-        tile = Button(root,text=game.grid[i][j],width=4,height=2,command = lambda: print("LEFT CLICK"))
+        tile = Button(root,text="",width=4,height=2)
 
         #to be removed: sets colour of mines to red
-        if game.grid[i][j]=="*":
-            tile.config(bg="red")
+        #if game.grid[i][j]=="*":
+        #    tile.config(bg="red")
         #####
 
         tile.bind("<Button-1>", lambda event, current_tile=tile, a=i, b=j: open_cell(current_tile,a,b))
+        tile.bind("<Button-2>", lambda event, current_tile=tile, a=i, b=j: confuse_cell(current_tile,a,b))
         tile.bind("<Button-3>", lambda event, current_tile=tile, a=i, b=j: flag_cell(current_tile,a,b))
         tile.grid(row=i,column=j)
 
