a9d6d19fe9f53e3e3295ee4c4ed966e8ca452398
Ru579 <shae.hewadewa@gmail.com>
2025-02-01 21:15:57 +0000

    Basic working database not organised

--- DatabaseHandler.py
+++ DatabaseHandler.py
@@ -1,3 +1,5 @@
+import datetime
+
 scores = {}
 
 
@@ -6,7 +8,13 @@ def add_user_info(username, time):  # where time is an array, 1st index is minut
         for line in file:
             record = line.split(":")
             scores[f"{record[0]}"] = record[1].strip("\n")
-    scores[f"{username}"] = f"{time[0]}-{time[1]}"
+    date = datetime.datetime.now().strftime("%x").split("/")
+    scores[f"{username}({date[1]}-{date[0]}-{date[2]})"] = f"{time[0]}-{time[1]}"
     with open("scores.txt", "w") as file:
         for a, b in scores.items():
             file.write(f"{a}:{b}\n")
+
+#TO-DO:
+#only add winners' scores to the database
+#organise database? shortest time at the top?
+#add menu, with option to view leaderboard
